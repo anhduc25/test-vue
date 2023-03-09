296adfb8d2970d6c5fe7d9404e896f7fb7caec29
anh duc <ducha181.work@gmail.com>
2023-03-09 11:32:40 +0700

    ok

--- BE/test.py
+++
@@ -1,9 +0,0 @@
-fake_users_db = {
-    "john": {
-        "username": "john",
-        "full_name": "John Doe",
-        "email": "john@example.com",
-        "hashed_password": "$2b$12$9QvD8WZH7Vq3NlIe7VrL/O8f1/6C5.VIuECDGcFOm8WJjrzHbgMy6"
-    }
-}
-print(fake_users_db["john"]['username'])
